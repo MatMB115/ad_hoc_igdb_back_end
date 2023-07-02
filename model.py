dd2da022e8996ce5464443a31ede5093eecad32f
MatMB115 <MatMB115@users.noreply.github.com>
2023-07-02 19:24:52 -0300

    (Add) order by e group by

--- model.py
+++ model.py
@@ -48,55 +48,102 @@ class API:
     def query_parse(self, body):
         query = ""
 
-        query += self.select_parse(body)
+        select, group = self.select_parse(body)
+
+        query += select
 
         query += self.join_parse(body)
 
         query += self.where_parse(body)
 
-        query += " limit 100"
+        query += group
+
+        query += self.order_parse(body)
+
+        if body['limit'] != None:
+            print(body['limit'])
+            query += f" limit {body['limit']}"
+
         print(query)
         return query
 
     def select_parse(self, body):
         select = body['select']
         select_fields = []
+        agreggate_fields = body['func_agregada']
+        group_field = []
         
         select_query = "SELECT "
+        group_query = " GROUP BY "
 
         if 'games' in select:
             for fields in select['games']:
-                select_fields.append("g." + fields + " AS game_" + fields)
+                if 'games' in agreggate_fields and agreggate_fields['games'][0] == fields:
+                    select_fields.append(agreggate_fields['games'][1] + "(g." + fields + ")"+ " AS game_" + agreggate_fields['games'][1] + "_" +fields)
+                else: 
+                    select_fields.append("g." + fields + " AS game_" + fields)
+                    group_field.append("g." + fields)
 
         if 'companies' in select:
             for fields in select['companies']:
-                select_fields.append("cp." + fields + " AS companies_" + fields)
+                if 'companies' in agreggate_fields and agreggate_fields['companies'][0] == fields:
+                    select_fields.append(agreggate_fields['companies'][1] + "(cp." + fields + ")"+ " AS companies_" + agreggate_fields['companies'][1] + "_" +fields)
+                else:
+                    select_fields.append("cp." + fields + " AS companies_" + fields)
+                    group_field.append("cp." + fields)
 
         if 'characters' in select:
             for fields in select['characters']:
-                select_fields.append("ch." + fields + " AS character_" + fields)
+                if 'characters' in agreggate_fields and agreggate_fields['characters'][0] == fields:
+                    select_fields.append(agreggate_fields['characters'][1] + "(ch." + fields + ")"+ " AS characters_" + agreggate_fields['characters'][1] + "_" +fields)
+                else:
+                    select_fields.append("ch." + fields + " AS characters_" + fields)
+                    group_field.append("ch." + fields)
 
         if 'plataforms' in select:
             for fields in select['plataforms']:
-                select_fields.append("p." + fields + " AS plataform_" + fields)
+                if 'plataforms' in agreggate_fields and agreggate_fields['plataforms'][0] == fields:
+                    select_fields.append(agreggate_fields['plataforms'][1] + "(p." + fields + ")"+ " AS plataforms_" + agreggate_fields['plataforms'][1] + "_" +fields)
+                else:
+                    select_fields.append("p." + fields + " AS plataforms_" + fields)
+                    group_field.append("p." + fields)
 
         if 'genres' in select:
             for fields in select['genres']:
-                select_fields.append("gr." + fields + " AS genres_" + fields)
+                if 'genres' in agreggate_fields and agreggate_fields['genres'][0] == fields:
+                    select_fields.append(agreggate_fields['genres'][1] + "(gr." + fields + ")"+ " AS genres_" + agreggate_fields['genres'][1] + "_" +fields)
+                else:
+                    select_fields.append("gr." + fields + " AS genres_" + fields)
+                    group_field.append("gr." + fields)
 
         if 'gameModes' in select:
             for fields in select['gameModes']:
-                select_fields.append("gmd." + fields + " AS games_modes_" + fields)
+                if 'gameModes' in agreggate_fields and agreggate_fields['gameModes'][0] == fields:
+                    select_fields.append(agreggate_fields['gameModes'][1] + "(gmd." + fields + ")"+ " AS gameModes_" + agreggate_fields['gameModes'][1] + fields)
+                else:
+                    select_fields.append("gmd." + fields + " AS gameModes_" + fields)
+                    group_field.append("gmd." + fields)
 
         if not select_fields:
-            return 'SELECT *'
+            return 'SELECT *', ''
 
         for field in select_fields[:-1]:
             select_query += field + ", "
 
         select_query += select_fields[-1]
+        
+        if body['group_by'] and len(group_field) >= 1:
+            for field in group_field[:-1]:
+                group_query += field + ", "
 
-        return select_query
+            group_query += group_field[-1]
+        else:
+            if (len(select_fields) - len(group_field)) != 0 and len(select_fields) > 1:
+                raise ValueError("Não é possível utilizar a função agregada com mais de um campo")
+            else:
+                group_query = ""
+
+        return select_query, group_query
 
 
     def join_parse(self, body):
@@ -148,7 +195,7 @@ class API:
         values = body['values']
         condition = body['condition']
         where_fields = []
-        where_query = " WHERE "
+        where_query = " WHERE"
 
         if 'games' in wheres:
             for where, operator, value in zip(wheres['games'], operators['games'], values['games']):
@@ -206,3 +253,29 @@ class API:
             return " " + table + where + " " + operator + " '" + str(value) + "' "
         else:
             return " " + table + where + " " + operator + " '%" + str(value) + "%'" + " "
+        
+    def order_parse(self, body):
+        order = body['order_by']
+        if order == None:
+            return ''
+        
+        order_query = " ORDER BY "
+        if 'games' in order:
+            order_query += "g." + order['games'][0] + " " + order['games'][1]
+
+        if 'companies' in order:
+            order_query += "cp." + order['companies'][0] + " " + order['companies'][1]
+
+        if 'characters' in order:
+            order_query += "ch." + order['characters'][0] + " " + order['characters'][1]
+
+        if 'plataforms' in order:
+            order_query += "p." + order['plataforms'][0] + " " + order['plataforms'][1]
+
+        if 'genres' in order:
+            order_query += "gr." + order['genres'][0] + " " + order['genres'][1]
+
+        if 'gameModes' in order:
+            order_query += "gmd." + order['gameModes'] + " " + order['gameModes'][1]
+
+        return order_query
